5158272a7dbec00992cac9f237d5f5e1f5a6c6ba
Lance Kao <717126@gmail,com>
2019-05-25 00:10:51 +0800

    Update version

--- Password_reset.py
+++ Password_reset.py
@@ -1,13 +1,12 @@
+
+password = 'a123456'
 rest_time = 2
 
-while True:
-	password = input('歡迎進入密碼重設頁面: ')
-	if password == 'a123456':
+while rest_time > 0 :
+	pwd = input('歡迎進入密碼重設頁面: ')
+	if pwd == password:
 		print('登入成功')
 		break
 	else:
-		print('密碼錯誤 ! 還有',rest_time,'次機會')
+		print('密碼錯誤 ! 還有', rest_time, '次機會')
 		rest_time = rest_time - 1 
-		if rest_time < 0:
-			print('錯誤達三次，系統自動關閉')
-			break
